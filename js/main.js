c57df15031697e980626023ce881dd18f4b2e1bd
Vishal Gawde <vrgawde1@gmail.com>
2019-10-15 18:51:24 +0530

    Added resume and reveal animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-$(window).on("load",function(){
-    $(".loader-wrapper").fadeOut("slow");
-});
+setTimeout(function() {
+	var time = 0;
+	var animate = ScrollReveal();
+	animate.reveal('.reveal-bottom', { distance: '100px', origin: 'bottom', delay: 250, interval: 250 });
+    animate.reveal('#reveal-right', { distance: '100px', origin: 'bottom', delay: 250, interval: 250 });
+    animate.reveal('#reveal-bottom', { distance: '100px', origin: 'bottom', delay: 250, interval: 250 });
+}, 3000);
